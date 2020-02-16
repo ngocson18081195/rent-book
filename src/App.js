ea4843eb60a3bd6321517dc2c68d6c293af7331d
nguyen ngoc son <sonnn@innotech-vn.com>
2020-02-16 12:08:56 +0700

    Implement layout Home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
-import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import Header from './Components/Header/Header';
+import Testimonials from './Components/Products/Testimonials';
 
 class App extends Component {
 
@@ -50,7 +50,7 @@ class App extends Component {
         {/* <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/> */}
-        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Testimonials data={this.state.resumeData.portfolio}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
