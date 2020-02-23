a488430c153d101de7efce0416004d2ebcd49d74
nguyen ngoc son <sonnn@innotech-vn.com>
2020-02-23 10:41:42 +0700

    Child Header.js

---
+++ src/Components/Header/ChildHeader/ChildHeader.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styles from './../styles.js';
+import { withStyles } from '@material-ui/core';
+
+class ChildHeader extends Component {
+
+    render() {
+        const { classes } = this.props;
+        if (this.props.data) {
+            var networks = this.props.data.social.map(function (network) {
+                return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+            })
+        }
+        return (
+            <div className="row banner">
+
+                <div className={classes.s01}>
+                    <form className={classes.s01Form}>
+                        <fieldset className={classes.fieldset}>
+                            <legend className={classes.legend}>Discover the Amazing City</legend>
+                        </fieldset>
+                        <div className={classes.innerForm}>
+                            <div className={classes.inputField, classes.firstWrap} >
+                                <input className={classes.input} id="search" type="text" placeholder="What are you looking for?" />
+                            </div>
+                            {/* <div class="input-field second-wrap">
+                        <input id="location" type="text" placeholder="location" />
+                     </div> */}
+                            <div className={classes.inputField, classes.thirdWrap}>
+                                <button className={classes.btnSearch} type="button">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(ChildHeader);
